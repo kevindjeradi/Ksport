9c6c6aeb32691e6859cbded738548665e35ba9ba
kevin <kevin-hamza.djeradi@epitech.eu>
2023-10-22 03:31:06 +0200

    changing training model in back

--- k_sport_back/models/training.js
+++ k_sport_back/models/training.js
@@ -4,11 +4,12 @@ const trainingSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: String,
     exercises: [{
+        label: String,
         exerciseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' },
         repetitions: Number,
         sets: Number,
         restTime: Number  // in seconds or minutes
     }],
-    targetMuscle: String,
     goal: String
 });
+module.exports = mongoose.model('Training', trainingSchema);
