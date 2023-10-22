2847e5eb45c5199b56eea8d8617ea675a0f32e81
kevin <kevin-hamza.djeradi@epitech.eu>
2023-10-22 03:36:05 +0200

    modifying training model and adding call to get all exercises in front

--- k_sport_front/lib/models/training.dart
+++ k_sport_front/lib/models/training.dart
@@ -2,20 +2,24 @@ class Training {
   final String id;
   final String name;
   final String description;
-  final int restTime;
+  final List<Map<String, dynamic>> exercises;
+  final String goal;
 
-  Training(
-      {required this.id,
-      required this.name,
-      required this.description,
-      required this.restTime});
+  Training({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.exercises,
+    required this.goal,
+  });
 
   factory Training.fromJson(Map<String, dynamic> json) {
     return Training(
       id: json['_id'],
       name: json['name'],
       description: json['description'],
-      restTime: json['restTime'],
+      exercises: List<Map<String, dynamic>>.from(json['exercises']),
+      goal: json['goal'],
     );
   }
 }
--- k_sport_front/lib/services/fetch_exercices.dart
+++ k_sport_front/lib/services/fetch_exercices.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:k_sport_front/models/exercices.dart';
 
-Future<List<Exercice>> fetchExercices(String muscleLabel) async {
+Future<List<Exercice>> fetchExercicesByMuscle(String muscleLabel) async {
   final response = await http.get(
       Uri.parse('http://10.0.2.2:3000/exercises?muscleLabel=$muscleLabel'));
 
@@ -10,6 +10,17 @@ Future<List<Exercice>> fetchExercices(String muscleLabel) async {
     List<dynamic> responseBody = json.decode(response.body);
     return responseBody.map((exercice) => Exercice.fromJson(exercice)).toList();
   } else {
-    throw Exception('Failed to load workouts');
+    throw Exception('Failed to load exercises');
+  }
+}
+
+Future<List<Exercice>> fetchExercices() async {
+  final response = await http.get(Uri.parse('http://10.0.2.2:3000/exercises'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> responseBody = json.decode(response.body);
+    return responseBody.map((exercice) => Exercice.fromJson(exercice)).toList();
+  } else {
+    throw Exception('Failed to load exercises');
   }
 }
