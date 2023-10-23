f7a45c4ab03e498866ccd79cc7fb67138cf49153
kevin <kevin-hamza.djeradi@epitech.eu>
2023-10-23 19:27:17 +0200

    updating exercise_card

--- k_sport_front/lib/components/trainings/exercise_card.dart
+++ k_sport_front/lib/components/trainings/exercise_card.dart
@@ -20,63 +20,71 @@ class ExerciseCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.only(bottom: 16.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: labelController,
-              textAlign: TextAlign.center,
-              decoration: const InputDecoration(
-                hintText: "Nom de l'exercice",
-                border: InputBorder.none,
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
               children: [
-                Flexible(
-                  child: TextField(
-                    controller: repsController,
-                    decoration: const InputDecoration(
-                      labelText: 'Répétitions',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.number,
+                TextField(
+                  controller: labelController,
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(
+                    hintText: "Nom de l'exercice",
+                    border: InputBorder.none,
                   ),
                 ),
-                const SizedBox(width: 8.0),
-                Flexible(
-                  child: TextField(
-                    controller: setsController,
-                    decoration: const InputDecoration(
-                      labelText: 'Séries',
-                      border: OutlineInputBorder(),
+                const SizedBox(height: 12.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        controller: repsController,
+                        decoration: const InputDecoration(
+                          labelText: 'Répétitions',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
                     ),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                const SizedBox(width: 8.0),
-                Flexible(
-                  child: TextField(
-                    controller: restTimeController,
-                    decoration: const InputDecoration(
-                      labelText: 'Repos (sec)',
-                      border: OutlineInputBorder(),
+                    const SizedBox(width: 8.0),
+                    Flexible(
+                      child: TextField(
+                        controller: setsController,
+                        decoration: const InputDecoration(
+                          labelText: 'Séries',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
                     ),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.remove_circle_outline),
-                  onPressed: onRemove,
-                  tooltip: 'Supprimer',
+                    const SizedBox(width: 8.0),
+                    Flexible(
+                      child: TextField(
+                        controller: restTimeController,
+                        decoration: const InputDecoration(
+                          labelText: 'Repos (sec)',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          Positioned(
+            top: 4,
+            right: 4,
+            child: IconButton(
+              icon: const Icon(Icons.remove_circle_outline),
+              onPressed: onRemove,
+              tooltip: 'Supprimer',
+            ),
+          ),
+        ],
       ),
     );
   }
