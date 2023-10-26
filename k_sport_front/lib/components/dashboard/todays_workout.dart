1b52c9574fddc68fde2573c9cc546cf86c50c9ee
kevin <kevin-hamza.djeradi@epitech.eu>
2023-10-26 13:20:47 +0200

    changing todays_workout component to have real data

--- k_sport_front/lib/components/dashboard/todays_workout.dart
+++ k_sport_front/lib/components/dashboard/todays_workout.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:k_sport_front/services/training_service.dart';
 
-class TodaysWorkout extends StatelessWidget {
-  // This is just a sample workout list. Replace it with dynamic data.
-  final List<Map<String, dynamic>> workouts = [
-    {'name': 'Pompes', 'series': 3, 'reps': 10},
-    {'name': 'Tractions', 'series': 3, 'reps': 15},
-    {'name': 'Squats', 'series': 3, 'reps': 15},
-    {'name': 'Curl', 'series': 3, 'reps': 15},
-  ];
+class TodaysWorkout extends StatefulWidget {
+  const TodaysWorkout({super.key});
 
-  TodaysWorkout({super.key});
+  @override
+  TodaysWorkoutState createState() => TodaysWorkoutState();
+}
+
+class TodaysWorkoutState extends State<TodaysWorkout> {
+  List<Map<String, dynamic>> workouts = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadWorkouts();
+  }
+
+  _loadWorkouts() async {
+    try {
+      await initializeDateFormatting('fr_FR', null);
+      String day =
+          DateFormat('EEEE', 'fr_FR').format(DateTime.now()).toLowerCase();
+      final training = await TrainingService.fetchTrainingForDay(day);
+      if (training != null) {
+        setState(() {
+          workouts = training.exercises
+              .map((exercise) => {
+                    'name': exercise['label'],
+                    'series': exercise['sets'],
+                    'reps': exercise['repetitions'],
+                  })
+              .toList();
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          workouts = [];
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print('Error: $e');
+      setState(() {
+        workouts = [];
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +59,24 @@ class TodaysWorkout extends StatelessWidget {
         const Text("Entraînement du jour", style: TextStyle(fontSize: 20)),
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: workouts.length,
-            itemBuilder: (context, index) {
-              final workout = workouts[index];
-              return ListTile(
-                leading: Image.network('https://via.placeholder.com/100x30',
-                    fit: BoxFit.cover, height: 30),
-                title: Text(workout['name']),
-                subtitle: Text(
-                    '${workout['series']} séries x ${workout['reps']} reps'),
-              );
-            },
-          ),
+          child: isLoading
+              ? CircularProgressIndicator()
+              : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: workouts.length,
+                  itemBuilder: (context, index) {
+                    final workout = workouts[index];
+                    return ListTile(
+                      leading: Image.network(
+                          'https://via.placeholder.com/100x30',
+                          fit: BoxFit.cover,
+                          height: 30),
+                      title: Text(workout['name']),
+                      subtitle: Text(
+                          '${workout['series']} séries x ${workout['reps']} reps'),
+                    );
+                  },
+                ),
         ),
       ],
     );
