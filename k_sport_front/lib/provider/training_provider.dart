b68e14f342aeb45bfcef9db943ca718c2e96e4a6
kevin <kevin-hamza.djeradi@epitech.eu>
2023-10-27 10:52:09 +0200

    updating provider for schedule

--- k_sport_front/lib/provider/training_provider.dart
+++ k_sport_front/lib/provider/training_provider.dart
@@ -2,14 +2,96 @@
 
 import 'package:flutter/material.dart';
 import 'package:k_sport_front/models/training.dart';
+import 'package:k_sport_front/services/training_service.dart';
 
 class TrainingProvider with ChangeNotifier {
-  final List<Training?> _weekTrainings = List.filled(7, null);
+  List<Training?> weekTrainings = List.filled(7, null);
+  List<Training> trainings = [];
+  bool isLoading = false;
+  String errorMessage = '';
+  static const List<String> dayNames = [
+    'Lundi',
+    'Mardi',
+    'Mercredi',
+    'Jeudi',
+    'Vendredi',
+    'Samedi',
+    'Dimanche'
+  ];
 
-  List<Training?> get weekTrainings => _weekTrainings;
+  Future<void> fetchTrainings() async {
+    try {
+      isLoading = true;
+      notifyListeners();
+      trainings = await TrainingService.fetchTrainings();
+      isLoading = false;
+      notifyListeners();
+    } catch (e) {
+      errorMessage = 'Error fetching trainings';
+      isLoading = false;
+      notifyListeners();
+    }
+  }
 
-  void setTrainingForDay(int day, Training? training) {
-    _weekTrainings[day - 1] = training;
+  Future<void> fetchTrainingForDay(String dayName, int dayIndex) async {
+    try {
+      isLoading = true;
+      notifyListeners();
+      Training? training = await TrainingService.fetchTrainingForDay(dayName);
+      weekTrainings[dayIndex] = training;
+      isLoading = false;
+      notifyListeners();
+    } catch (e) {
+      errorMessage = "Error fetching training for day $dayName";
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  updateTrainingForDay(int dayIndex, Training? training) {
+    weekTrainings[dayIndex] = training;
     notifyListeners();
+  }
+
+  fetchAllTrainingsForTheWeek() async {
+    try {
+      isLoading = true;
+      notifyListeners();
+      for (int index = 0; index < 7; index++) {
+        await fetchTrainingForDay(dayNames[index], index);
+      }
+      isLoading = false;
+      notifyListeners();
+    } catch (e) {
+      errorMessage = "Error fetching trainings for the week";
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  List<Map<String, dynamic>> get todayWorkouts {
+    // Assuming weekTrainings has the training data for each day of the week
+    final today = DateTime.now().weekday -
+        1; // DateTime.now().weekday returns 1 for Monday, 2 for Tuesday, etc.
+    final trainingOfToday = weekTrainings[today];
+
+    if (trainingOfToday != null) {
+      return trainingOfToday.exercises
+          .map((exercise) => {
+                'name': exercise['label'],
+                'series': exercise['sets'],
+                'reps': exercise['repetitions'],
+              })
+          .toList();
+    }
+    return [];
+  }
+
+  Training? getTrainingForDay(String dayName) {
+    int dayIndex = dayNames.indexOf(dayName);
+    if (dayIndex != -1) {
+      return weekTrainings[dayIndex];
+    }
+    return null;
   }
 }
