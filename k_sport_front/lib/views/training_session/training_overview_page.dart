a5ebb2009576d63b616af71da270f7c429656e42
kevin <kevin-hamza.djeradi@epitech.eu>
2023-10-30 19:38:22 +0100

    refactoring TrainingOverviewPage to a statefull widget, adding navigation a exercise detail when user click on an exercice in training overview, adding card to show preview of how long the training will last approximately

--- k_sport_front/lib/views/training_session/training_overview_page.dart
+++ k_sport_front/lib/views/training_session/training_overview_page.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:k_sport_front/components/exercices/workout_card_detail.dart';
+import 'package:k_sport_front/components/generic/custom_navigation.dart';
 import 'package:k_sport_front/components/generic/cutom_elevated_button.dart';
 import 'package:k_sport_front/components/navigation/return_app_bar.dart';
 import 'package:k_sport_front/models/training.dart';
+import 'package:k_sport_front/services/api.dart';
 import 'package:k_sport_front/views/training_session/training_session_page.dart';
 
-class TrainingOverviewPage extends StatelessWidget {
+class TrainingOverviewPage extends StatefulWidget {
   final Training training;
 
   const TrainingOverviewPage({Key? key, required this.training})
       : super(key: key);
 
   @override
+  TrainingOverviewPageState createState() => TrainingOverviewPageState();
+}
+
+class TrainingOverviewPageState extends State<TrainingOverviewPage> {
+  int calculateTrainingDuration(Training training) {
+    const int secondsPerRepetition = 3;
+    const int additionalActivityTime =
+        180; // temps de transition entre les exos
+
+    int totalDurationInSeconds = 0;
+
+    for (var exercise in training.exercises) {
+      int sets = exercise['sets'];
+      int repetitions = exercise['repetitions'];
+      int restTimePerSet = exercise['restTime'];
+      totalDurationInSeconds +=
+          sets * (repetitions * secondsPerRepetition + restTimePerSet);
+    }
+
+    totalDurationInSeconds += additionalActivityTime;
+    return (totalDurationInSeconds / 60).ceil();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    print("\n\n\nNom de l'entrainement -> ${training.name}\n\n\n");
-    print("\n\n\nNombre d'exos -> ${training.exercises.length}\n");
-    print("\n\n\nNom de l'exercice 1 -> ${training.exercises[0]['label']}\n");
-    print("\n\n\nNombre de series exo 1 -> ${training.exercises[0]['sets']}\n");
-    print(
-        "\n\n\nNombres de series -> ${training.exercises[0]['repetitions']}\n\n");
+    const int showerTime = 15;
+    final int trainingDuration = calculateTrainingDuration(widget.training);
 
     return Scaffold(
       appBar: const ReturnAppBar(
@@ -29,11 +52,58 @@ class TrainingOverviewPage extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
+              Card(
+                elevation: 5,
+                shadowColor: Colors.blue.withOpacity(0.5),
+                margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          "Séance ${widget.training.name}",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Row(
+                        children: [
+                          Icon(Icons.shower, color: Colors.blue),
+                          SizedBox(width: 8),
+                          Text(
+                            'Douche: $showerTime minutes',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          const Icon(Icons.timer, color: Colors.blue),
+                          const SizedBox(width: 8),
+                          Text(
+                            'Durée totale estimée: ${trainingDuration + showerTime} minutes',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               Expanded(
                 child: ListView.builder(
-                  itemCount: training.exercises.length,
+                  itemCount: widget.training.exercises.length,
                   itemBuilder: (context, index) {
-                    final exercise = training.exercises[index];
+                    final exercise = widget.training.exercises[index];
                     return SizedBox(
                       height: MediaQuery.of(context).size.height * 0.2,
                       child: Card(
@@ -46,9 +116,33 @@ class TrainingOverviewPage extends StatelessWidget {
                         ),
                         child: InkWell(
                           borderRadius: BorderRadius.circular(12),
-                          onTap: () {
-                            // Handle tap
+                          onTap: () async {
                             print('Exercise tapped: ${exercise['label']}');
+                            print('Exercise tapped: ${exercise['_id']}');
+
+                            final exerciseLabel = exercise['label'];
+                            if (exerciseLabel != null) {
+                              final exerciseDetails =
+                                  await Api.fetchExerciseDetailsByLabel(
+                                      exerciseLabel);
+                              if (exerciseDetails != null && mounted) {
+                                CustomNavigation.push(
+                                    context,
+                                    WorkoutCardDetail(
+                                      title:
+                                          exerciseDetails['label'] ?? 'label',
+                                      muscleLabel:
+                                          exerciseDetails['muscleLabel'] ??
+                                              'muscleLabel',
+                                      image: Image.network(exerciseDetails[
+                                              'imageUrl'] ??
+                                          'https://via.placeholder.com/150'),
+                                      description: exerciseDetails[
+                                              'detailDescription'] ??
+                                          'No description',
+                                    ));
+                              }
+                            }
                           },
                           child: Padding(
                             padding: const EdgeInsets.all(12),
