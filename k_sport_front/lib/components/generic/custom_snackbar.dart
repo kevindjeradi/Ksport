99685c14aa81b46e990b149331e4c3c9987a32c4
kevin <kevin-hamza.djeradi@epitech.eu>
2023-10-31 12:58:03 +0100

    updating the snackbar ui, with icon and preventing spamming snackbar apparitions

--- k_sport_front/lib/components/generic/custom_snackbar.dart
+++ k_sport_front/lib/components/generic/custom_snackbar.dart
@@ -5,31 +5,46 @@ void showCustomSnackBar(
   final theme = Theme.of(context);
   Color backgroundColor;
   Color textColor;
+  IconData? iconData;
 
   switch (type) {
     case SnackBarType.error:
       backgroundColor = theme.colorScheme.error;
       textColor = theme.colorScheme.onError;
+      iconData = Icons.error_outline;
       break;
     case SnackBarType.success:
-      backgroundColor = theme.colorScheme.secondary;
-      textColor = theme.colorScheme.onSecondary;
+      backgroundColor = theme.colorScheme.secondaryContainer;
+      textColor = theme.colorScheme.onSecondaryContainer;
+      iconData = Icons.check_circle_outline;
       break;
     case SnackBarType.info:
     default:
-      backgroundColor = theme.colorScheme.secondaryContainer;
-      textColor = theme.colorScheme.onSurface;
+      backgroundColor = theme.colorScheme.primary;
+      textColor = theme.colorScheme.onPrimary;
+      iconData = Icons.info_outline;
       break;
   }
 
   final snackBar = SnackBar(
     duration: const Duration(seconds: 1),
-    content: Text(
-      message,
-      style: TextStyle(
-        color: textColor,
-        fontSize: 16.0,
-      ),
+    content: Row(
+      children: [
+        Icon(
+          iconData,
+          color: textColor,
+        ),
+        const SizedBox(width: 8.0),
+        Expanded(
+          child: Text(
+            message,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ],
     ),
     backgroundColor: backgroundColor,
     behavior: SnackBarBehavior.floating,
@@ -40,7 +55,9 @@ void showCustomSnackBar(
     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
   );
 
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(snackBar);
 }
 
 enum SnackBarType { info, error, success }
