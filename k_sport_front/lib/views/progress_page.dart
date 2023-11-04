001083a5bda219d312470c28ed956f81db14595a
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-04 19:33:47 +0100

    adding CompletedTrainings in progress_page

--- k_sport_front/lib/views/progress_page.dart
+++ k_sport_front/lib/views/progress_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:k_sport_front/components/history/completed_training.dart';
 
 class ProgressPage extends StatelessWidget {
   const ProgressPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: theme.colorScheme.background,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Mes Progrès",
+                  style: theme.textTheme.displaySmall,
+                ),
+              ),
+              const Expanded(child: CompletedTrainings()),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
