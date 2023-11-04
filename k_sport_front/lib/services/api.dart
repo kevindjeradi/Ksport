2470abcadf35409b91a34d4cd3431ebe365d9e85
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-04 02:37:23 +0100

    refactoring api to hav logs in handleRequest

--- k_sport_front/lib/services/api.dart
+++ k_sport_front/lib/services/api.dart
@@ -1,6 +1,8 @@
 // api.dart
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:k_sport_front/helpers/logger.dart';
 import 'package:k_sport_front/models/exercices.dart';
 import 'package:k_sport_front/models/muscles.dart';
 import 'package:k_sport_front/provider/user_provider.dart';
@@ -10,56 +12,69 @@ import 'package:k_sport_front/services/user_service.dart';
 class Api {
   static final TokenService _tokenService = TokenService();
   static final _userService = UserService();
+  static final String baseUrl = dotenv.env['API_URL'] ??
+      'http://10.0.2.2:3000'; // Default URL if .env is not loaded
 
-  static Future<http.Response> get(String url) async {
-    final token = await _tokenService.getToken();
-    print("\n--------------token in Api.get: $token");
-    return http.get(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
+  Future<http.Response> _handleRequest(
+      Future<http.Response> Function() action) async {
+    try {
+      final response = await action();
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return response;
+      } else {
+        Log.logger.e('Request failed with status: ${response.statusCode}');
+        throw Exception('Failed to complete request');
+      }
+    } catch (e, s) {
+      Log.logger.e('Error: $e\nStack trace: $s');
+      throw Exception('Failed to complete request');
+    }
   }
 
-  static Future<http.Response> post(
-      String url, Map<String, dynamic> data) async {
+  Future<http.Response> get(String url) async {
     final token = await _tokenService.getToken();
-    print("\n--------------token in post $token");
-    return http.post(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-      body: jsonEncode(data),
-    );
+    return _handleRequest(() => http.get(
+          Uri.parse(url),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+        ));
   }
 
-  static Future<http.Response> put(
-      String url, Map<String, dynamic> data) async {
+  Future<http.Response> post(String url, Map<String, dynamic> data) async {
     final token = await _tokenService.getToken();
-    return http.put(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-      body: jsonEncode(data),
-    );
+    return _handleRequest(() => http.post(
+          Uri.parse(url),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          body: jsonEncode(data),
+        ));
   }
 
-  static Future<http.Response> delete(String url) async {
+  Future<http.Response> put(String url, Map<String, dynamic> data) async {
     final token = await _tokenService.getToken();
-    print("\n--------------token in Api.delete: $token");
-    return http.delete(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
+    return _handleRequest(() => http.put(
+          Uri.parse(url),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          body: jsonEncode(data),
+        ));
+  }
+
+  Future<http.Response> delete(String url) async {
+    final token = await _tokenService.getToken();
+    return _handleRequest(() => http.delete(
+          Uri.parse(url),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+        ));
   }
 
   static Future<void> populateUserProvider(UserProvider userProvider) async {
@@ -70,14 +85,13 @@ class Api {
 
       // Populate the UserProvider with the fetched details
       userProvider.setUserData(userDetails);
-    } catch (error) {
-      print("Error populating UserProvider: $error");
+    } catch (e) {
+      Log.logger.e("Error populating UserProvider");
     }
   }
 
-  static Future<List<String>> fetchMusclesByGroup(String group) async {
-    final response =
-        await get('http://10.0.2.2:3000/muscles/byGroup?group=$group');
+  Future<List<String>> fetchMusclesByGroup(String group) async {
+    final response = await get('$baseUrl/muscles/byGroup?group=$group');
 
     if (response.statusCode == 200) {
       List<dynamic> responseBody = json.decode(response.body);
@@ -86,23 +100,23 @@ class Api {
           responseBody.map((muscle) => muscle['label'] as String).toList();
       return muscleLabels;
     } else {
-      throw Exception('Failed to load muscles');
+      throw Exception('Failed to load muscles by group -> group: $group');
     }
   }
 
-  static Future<List<Muscle>> fetchMuscles() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:3000/muscles'));
+  Future<List<Muscle>> fetchMuscles() async {
+    final response = await get('$baseUrl/muscles');
 
     if (response.statusCode == 200) {
       List<dynamic> responseBody = json.decode(response.body);
       return responseBody.map((muscle) => Muscle.fromJson(muscle)).toList();
     } else {
-      throw Exception('Failed to load workouts');
+      throw Exception('Failed to fetch Muscles');
     }
   }
 
-  static Future<void> addMuscle(Muscle muscle) async {
-    const url = 'http://10.0.2.2:3000/muscles';
+  Future<void> addMuscle(Muscle muscle) async {
+    final url = '$baseUrl/muscles';
     final response = await post(
       url,
       {
@@ -118,10 +132,8 @@ class Api {
     }
   }
 
-  static Future<List<Exercice>> fetchExercisesByMuscle(
-      String muscleLabel) async {
-    final response = await http.get(
-        Uri.parse('http://10.0.2.2:3000/exercises?muscleLabel=$muscleLabel'));
+  Future<List<Exercice>> fetchExercisesByMuscle(String muscleLabel) async {
+    final response = await get('$baseUrl/exercises?muscleLabel=$muscleLabel');
 
     if (response.statusCode == 200) {
       List<dynamic> responseBody = json.decode(response.body);
@@ -129,37 +141,32 @@ class Api {
           .map((exercice) => Exercice.fromJson(exercice))
           .toList();
     } else {
-      throw Exception('Failed to load exercises');
+      throw Exception(
+          'Failed to load exercises by muscle -> muscle: $muscleLabel');
     }
   }
 
-  static Future<Map<String, dynamic>?> fetchExerciseDetailsByLabel(
+  Future<Map<String, dynamic>?> fetchExerciseDetailsByLabel(
       String exerciseLabel) async {
-    try {
-      final response =
-          await get('http://10.0.2.2:3000/exercises/label/$exerciseLabel');
+    final response = await get('$baseUrl/exercises/label/$exerciseLabel');
 
-      if (response.statusCode == 200) {
-        return json.decode(response.body);
-      } else {
-        print('Failed to load exercise details: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error: $e');
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      Log.logger.e(
+          'Failed to load exercise details by label -> exerciseLabel: $exerciseLabel');
     }
     return null;
   }
 
-  static Future<List<Exercice>> fetchExercisesByMuscleGroup(
-      String group) async {
+  Future<List<Exercice>> fetchExercisesByMuscleGroup(String group) async {
     // Step 1: Fetch muscle labels by group
     List<String> muscleLabels = await fetchMusclesByGroup(group);
 
     // Step 2: Fetch exercises by muscle labels
     List<Exercice> exercises = [];
     for (String label in muscleLabels) {
-      final response = await http
-          .get(Uri.parse('http://10.0.2.2:3000/exercises?muscleLabel=$label'));
+      final response = await get('$baseUrl/exercises?muscleLabel=$label');
 
       if (response.statusCode == 200) {
         List<dynamic> responseBody = json.decode(response.body);
@@ -167,53 +174,43 @@ class Api {
             .map((exercise) => Exercice.fromJson(exercise))
             .toList());
       } else {
-        throw Exception('Failed to load exercises for muscle label: $label');
+        throw Exception(
+            'Failed to fetch exercises by muscle group -> label: $label / group: $group');
       }
     }
     return exercises;
   }
 
   static Future<List<Map<String, dynamic>>> fetchExercises() async {
-    final response =
-        await http.get(Uri.parse('http://10.0.2.2:3000/exercises'));
+    final response = await http.get(Uri.parse('$baseUrl/exercises'));
     if (response.statusCode == 200) {
       return List<Map<String, dynamic>>.from(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load exercises');
+      throw Exception('Failed to fetch exercises');
     }
   }
 
-  static Future<http.Response> postTraining(Map<String, dynamic> data) {
-    return post('http://10.0.2.2:3000/trainings', data);
+  Future<http.Response> postTraining(Map<String, dynamic> data) {
+    return post('$baseUrl/trainings', data);
   }
 
-  static Future<http.Response> updateTraining(
-      String id, Map<String, dynamic> data) {
-    return put('http://10.0.2.2:3000/trainings/$id', data);
+  Future<http.Response> updateTraining(String id, Map<String, dynamic> data) {
+    return put('$baseUrl/trainings/$id', data);
   }
 
-  static Future<Map<String, dynamic>> fetchTrainingForDay(String day) async {
-    final response =
-        await get('http://10.0.2.2:3000/user/getTrainingForDay/$day');
+  Future<Map<String, dynamic>> fetchTrainingForDay(String day) async {
+    final response = await get('$baseUrl/user/getTrainingForDay/$day');
     if (response.statusCode == 200) {
       return Map<String, dynamic>.from(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load training for $day');
+      throw Exception('Failed to load training for day -> day: $day');
     }
   }
 
-  static Future<void> deleteTrainingForDay(String day) async {
-    final token = await _tokenService.getToken();
-
-    final response = await http.delete(
-      Uri.parse('http://10.0.2.2:3000/user/deleteTrainingForDay/$day'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
+  Future<void> deleteTrainingForDay(String day) async {
+    final response = await delete('$baseUrl/user/deleteTrainingForDay/$day');
     if (response.statusCode != 200) {
-      throw Exception('Failed to delete training for $day');
+      throw Exception('Failed to delete training for day -> day: $day');
     }
   }
 }
