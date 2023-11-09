d434f234d47cfe45686f8635444ed46e42f2bb2d
Kevin Hamza Djeradi <kevin@Mac-mini-de-Kevin.local>
2023-11-09 12:25:32 +0100

    change server listen

--- k_sport_back/server.js
+++ k_sport_back/server.js
@@ -34,6 +34,7 @@ app.use('/', musclesRoutes);
 app.use('/', exercicesRoutes);
 app.use('/', checkAuth, trainingRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Change from 'localhost' to '0.0.0.0' to listen on all network interfaces
+app.listen(PORT, '0.0.0.0', function() {
+  console.log(`Server running on port ${PORT}`);
 });
