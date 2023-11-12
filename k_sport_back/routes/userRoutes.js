065252c899223a7b2b37f95aa4a3032208d20832
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-12 21:45:42 +0100

    fix record completed training in userRoutes in back

--- k_sport_back/routes/userRoutes.js
+++ k_sport_back/routes/userRoutes.js
@@ -250,18 +250,43 @@ router.post('/user/recordCompletedTraining', async (req, res) => {
     try {
         const { trainingId, dateCompleted } = req.body;
         const token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(token, JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Replace with your JWT secret
         const userId = decoded.userId;
+
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
+
         if (!mongoose.Types.ObjectId.isValid(trainingId)) {
             return res.status(400).json({ error: 'Invalid trainingId format' });
         }
-        const newCompletedTraining = { trainingId, dateCompleted };
+
+        const training = await Training.findById(trainingId);
+        if (!training) {
+            return res.status(404).json({ error: 'Training not found' });
+        }
+
+        const newCompletedTraining = { 
+            trainingData: {
+                name: training.name,
+                description: training.description,
+                exercises: training.exercises.map(ex => ({
+                    label: ex.label,
+                    exerciseId: ex.exerciseId,
+                    repetitions: ex.repetitions,
+                    sets: ex.sets,
+                    weight: ex.weight,
+                    restTime: ex.restTime
+                })),
+                goal: training.goal,
+            },
+            dateCompleted: new Date(dateCompleted) // assuming dateCompleted is in a valid format
+        };
+
         user.history.completedTrainings.push(newCompletedTraining);
         await user.save();
+
         res.status(200).json({ message: 'Training recorded successfully' });
     } catch (error) {
         console.log("recordCompletedTraining error: " + error.message)
