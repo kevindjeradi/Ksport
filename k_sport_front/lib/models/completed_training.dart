79a9cea15f288f414a03b6c10af303a964007f5b
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-13 18:47:45 +0100

    update completed training to hold all informations about the training instead of a training id

--- k_sport_front/lib/models/completed_training.dart
+++ k_sport_front/lib/models/completed_training.dart
@@ -1,26 +1,62 @@
+// In completed_training.dart
 class CompletedTraining {
   final String trainingId;
   final DateTime dateCompleted;
-  final String id;
+  final String name;
+  final String description;
+  final List<TrainingExercise> exercises;
+  final String goal;
 
-  CompletedTraining(
-      {required this.trainingId,
-      required this.dateCompleted,
-      required this.id});
+  CompletedTraining({
+    required this.trainingId,
+    required this.dateCompleted,
+    required this.name,
+    required this.description,
+    required this.exercises,
+    required this.goal,
+  });
 
   factory CompletedTraining.fromMap(Map<String, dynamic> map) {
     return CompletedTraining(
-      trainingId: map['trainingId'],
-      dateCompleted: DateTime.parse(map['dateCompleted']),
-      id: map['_id'],
+      trainingId: map['_id'] ?? 'default-training-id',
+      dateCompleted: map['dateCompleted'] != null
+          ? DateTime.parse(map['dateCompleted'])
+          : DateTime.now(),
+      name: map['trainingData']['name'] ?? 'Default Name',
+      description: map['trainingData']['description'] ?? 'Default Description',
+      exercises: (map['trainingData']['exercises'] as List? ?? [])
+          .map((e) => TrainingExercise.fromMap(e as Map<String, dynamic>))
+          .toList(),
+      goal: map['trainingData']['goal'] ?? 'Default Goal',
     );
   }
+}
 
-  Map<String, dynamic> toMap() {
-    return {
-      'trainingId': trainingId,
-      'dateCompleted': dateCompleted.toIso8601String(),
-      '_id': id,
-    };
+class TrainingExercise {
+  final String label;
+  final String exerciseId;
+  final List<int> repetitions;
+  final int sets;
+  final List<int> weight;
+  final List<int> restTime;
+
+  TrainingExercise({
+    required this.label,
+    required this.exerciseId,
+    required this.repetitions,
+    required this.sets,
+    required this.weight,
+    required this.restTime,
+  });
+
+  factory TrainingExercise.fromMap(Map<String, dynamic> map) {
+    return TrainingExercise(
+      label: map['label'] ?? 'Default Label',
+      exerciseId: map['exerciseId'] ?? 'default-exercise-id',
+      repetitions: List<int>.from(map['repetitions'] ?? []),
+      sets: map['sets'] ?? 0,
+      weight: List<int>.from(map['weight'] ?? []),
+      restTime: List<int>.from(map['restTime'] ?? []),
+    );
   }
 }
