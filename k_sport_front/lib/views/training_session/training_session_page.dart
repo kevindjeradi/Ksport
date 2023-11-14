dc5846a700b6d8f0d503c578a6429dfcb6820efb
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-14 02:32:27 +0100

    Changed inforow to have a boolean if it's editable. When it's true there is an icon and the row is clickable

--- k_sport_front/lib/views/training_session/training_session_page.dart
+++ k_sport_front/lib/views/training_session/training_session_page.dart
@@ -369,9 +369,11 @@ class ExerciseInfoCard extends StatelessWidget {
               label: 'Séries',
               value: "$currentSet / ${exercise['series'].toString()}",
             ),
-            GestureDetector(
+            InfoRow(
+              label: 'Répétitions',
+              value: repsForCurrentSet,
+              isEditable: true,
               onTap: () => _updateReps(context),
-              child: InfoRow(label: 'Répétitions', value: repsForCurrentSet),
             ),
             InfoRow(
                 label: 'Repos',
@@ -387,23 +389,42 @@ class ExerciseInfoCard extends StatelessWidget {
 class InfoRow extends StatelessWidget {
   final String label;
   final String value;
+  final VoidCallback? onTap;
+  final bool isEditable;
 
-  const InfoRow({Key? key, required this.label, required this.value})
-      : super(key: key);
+  const InfoRow({
+    Key? key,
+    required this.label,
+    required this.value,
+    this.onTap,
+    this.isEditable = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Text(label, style: theme.textTheme.titleLarge),
-          ),
-          Text(value, style: theme.textTheme.headlineSmall),
-        ],
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(label, style: theme.textTheme.titleLarge),
+            ),
+            if (isEditable)
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.edit, size: 18, color: theme.primaryColor),
+                  const SizedBox(width: 4),
+                  Text(value, style: theme.textTheme.headlineSmall),
+                ],
+              )
+            else
+              Text(value, style: theme.textTheme.headlineSmall),
+          ],
+        ),
       ),
     );
   }
