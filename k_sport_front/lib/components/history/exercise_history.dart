445ec83b6154cff7c6d5dbb39f5b3049bb0b9a54
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-15 19:02:05 +0100

    refactoring line chart into a component

--- k_sport_front/lib/components/history/exercise_history.dart
+++ k_sport_front/lib/components/history/exercise_history.dart
@@ -1,9 +1,8 @@
 // exercise_history.dart
-import 'dart:math';
-
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:k_sport_front/components/history/bar_charts/custom_bar_chart.dart';
+import 'package:k_sport_front/components/history/line_charts/exercise_line_chart.dart';
 import 'package:k_sport_front/components/navigation/return_app_bar.dart';
 import 'package:k_sport_front/provider/user_provider.dart';
 import 'package:provider/provider.dart';
@@ -69,30 +68,30 @@ class ExerciseHistory extends StatelessWidget {
               chartTitle: 'Evolution du poids soulevé',
               textColor: theme.colorScheme.onSecondary,
             ),
-            _buildLineChart(
-              _prepareLineChartData(totalWeightsList),
-              'Evolution du poids soulevé',
-              theme.colorScheme.onSecondary,
+            ExerciseLineChart(
+              dataList: totalWeightsList,
+              chartTitle: 'Evolution du poids soulevé',
+              textColor: theme.colorScheme.onSecondary,
             ),
             CustomBarChart(
               barData: setsBarData,
               chartTitle: 'Evolution du nombre de repetitions',
               textColor: theme.colorScheme.onSecondary,
             ),
-            _buildLineChart(
-              _prepareLineChartData(totalRepsList),
-              'Evolution du nombre de repetitions',
-              theme.colorScheme.onSecondary,
+            ExerciseLineChart(
+              dataList: totalRepsList,
+              chartTitle: 'Evolution du nombre de repetitions',
+              textColor: theme.colorScheme.onSecondary,
             ),
             CustomBarChart(
               barData: restTimeBarData,
               chartTitle: 'Evolution du temps de repos',
               textColor: theme.colorScheme.onSecondary,
             ),
-            _buildLineChart(
-              _prepareLineChartData(totalRestTimesList),
-              'Evolution du temps de repos',
-              theme.colorScheme.onSecondary,
+            ExerciseLineChart(
+              dataList: totalRestTimesList,
+              chartTitle: 'Evolution du temps de repos',
+              textColor: theme.colorScheme.onSecondary,
             ),
           ],
         ),
@@ -114,121 +113,4 @@ class ExerciseHistory extends StatelessWidget {
       );
     }).toList();
   }
-}
-
-List<FlSpot> _prepareLineChartData(List<int> dataList) {
-  return dataList
-      .asMap()
-      .entries
-      .map((entry) => FlSpot(entry.key.toDouble(), entry.value.toDouble()))
-      .toList();
-}
-
-Widget _buildLineChart(List<FlSpot> spots, String chartTitle, Color textColor) {
-  return Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: Card(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              chartTitle,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-            child: SizedBox(
-              height: 200,
-              child: LineChart(
-                LineChartData(
-                  lineBarsData: [
-                    LineChartBarData(
-                      spots: spots,
-                      isCurved: true,
-                      color: Colors.blue,
-                      barWidth: 3,
-                      isStrokeCapRound: true,
-                      dotData: const FlDotData(show: false),
-                      belowBarData: BarAreaData(show: false),
-                    ),
-                  ],
-                  gridData: FlGridData(
-                    show: true,
-                    drawVerticalLine: false,
-                    drawHorizontalLine: true,
-                    getDrawingVerticalLine: (value) {
-                      return const FlLine(
-                        color: Color(0xff37434d),
-                        strokeWidth: 1,
-                      );
-                    },
-                  ),
-                  titlesData: FlTitlesData(
-                    leftTitles: const AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
-                    bottomTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 22,
-                        getTitlesWidget: (value, meta) {
-                          return SideTitleWidget(
-                            axisSide: meta.axisSide,
-                            space: 8.0,
-                            child: Text(
-                              value.toInt().toString(),
-                              style: TextStyle(
-                                  color: textColor,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    topTitles: const AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
-                    rightTitles: const AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
-                  ),
-                  borderData: FlBorderData(
-                    show: false,
-                    border:
-                        Border.all(color: const Color(0xff37434d), width: 1),
-                  ),
-                  minX: 0,
-                  maxX: (spots.length - 1).toDouble(),
-                  minY: 0,
-                  maxY:
-                      spots.map((spot) => spot.y).reduce(max).toDouble() * 1.2,
-                  lineTouchData: LineTouchData(
-                    touchTooltipData: LineTouchTooltipData(
-                      tooltipBgColor: Colors.blueAccent,
-                      getTooltipItems: (touchedSpots) {
-                        return touchedSpots.map((touchedSpot) {
-                          return LineTooltipItem(
-                            '${touchedSpot.y.toInt()}',
-                            TextStyle(color: textColor),
-                          );
-                        }).toList();
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
 }
---
+++ k_sport_front/lib/components/history/line_charts/exercise_line_chart.dart
@@ -0,0 +1,136 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class ExerciseLineChart extends StatelessWidget {
+  final List<int> dataList;
+  final String chartTitle;
+  final Color textColor;
+
+  const ExerciseLineChart({
+    Key? key,
+    required this.dataList,
+    required this.chartTitle,
+    required this.textColor,
+  }) : super(key: key);
+
+  List<FlSpot> _prepareLineChartData(List<int> dataList) {
+    return dataList
+        .asMap()
+        .entries
+        .map((entry) => FlSpot(entry.key.toDouble(), entry.value.toDouble()))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<FlSpot> spots = _prepareLineChartData(dataList);
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                chartTitle,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+              child: SizedBox(
+                height: 200,
+                child: LineChart(
+                  LineChartData(
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: spots,
+                        isCurved: true,
+                        color: Colors.blue,
+                        barWidth: 3,
+                        isStrokeCapRound: true,
+                        dotData: const FlDotData(show: false),
+                        belowBarData: BarAreaData(show: false),
+                      ),
+                    ],
+                    gridData: FlGridData(
+                      show: true,
+                      drawVerticalLine: false,
+                      drawHorizontalLine: true,
+                      getDrawingVerticalLine: (value) {
+                        return const FlLine(
+                          color: Color(0xff37434d),
+                          strokeWidth: 1,
+                        );
+                      },
+                    ),
+                    titlesData: FlTitlesData(
+                      leftTitles: const AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 22,
+                          getTitlesWidget: (value, meta) {
+                            return SideTitleWidget(
+                              axisSide: meta.axisSide,
+                              space: 8.0,
+                              child: Text(
+                                value.toInt().toString(),
+                                style: TextStyle(
+                                    color: textColor,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      topTitles: const AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      rightTitles: const AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                    ),
+                    borderData: FlBorderData(
+                      show: false,
+                      border:
+                          Border.all(color: const Color(0xff37434d), width: 1),
+                    ),
+                    minX: 0,
+                    maxX: (spots.length - 1).toDouble(),
+                    minY: 0,
+                    maxY: spots.map((spot) => spot.y).reduce(max).toDouble() *
+                        1.2,
+                    lineTouchData: LineTouchData(
+                      touchTooltipData: LineTouchTooltipData(
+                        tooltipBgColor: Colors.blueAccent,
+                        getTooltipItems: (touchedSpots) {
+                          return touchedSpots.map((touchedSpot) {
+                            return LineTooltipItem(
+                              '${touchedSpot.y.toInt()}',
+                              TextStyle(color: textColor),
+                            );
+                          }).toList();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
