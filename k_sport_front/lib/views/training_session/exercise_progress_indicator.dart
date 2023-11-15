c6734bd2a40a3cfe79c9605702df531bc11a09f2
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-15 20:57:01 +0100

    updated exercise progress indicator to be based on total number of sets instead of number of exercises + removed next exercise text

--- k_sport_front/lib/views/training_session/exercise_progress_indicator.dart
+++ k_sport_front/lib/views/training_session/exercise_progress_indicator.dart
@@ -2,27 +2,37 @@ import 'package:flutter/material.dart';
 
 class ExerciseProgressIndicator extends StatelessWidget {
   final int currentExerciseIndex;
-  final int totalExercises;
-  final String nextExerciseName;
+  final int currentSet;
+  final List<int> setsPerExercise;
 
   const ExerciseProgressIndicator({
     Key? key,
     required this.currentExerciseIndex,
-    required this.totalExercises,
-    this.nextExerciseName = "",
+    required this.currentSet,
+    required this.setsPerExercise,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
+    double calculateProgress() {
+      int completedSets = setsPerExercise
+              .take(currentExerciseIndex - 1)
+              .fold(0, (sum, element) => sum + element) +
+          currentSet;
+      int totalSetsOverall =
+          setsPerExercise.fold(0, (sum, element) => sum + element);
+
+      return completedSets / totalSetsOverall.toDouble();
+    }
+
     return Column(
       children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(10),
           child: LinearProgressIndicator(
-            value:
-                totalExercises > 0 ? currentExerciseIndex / totalExercises : 0,
+            value: calculateProgress(),
             minHeight: 20,
             backgroundColor: theme.colorScheme.background,
             valueColor:
@@ -30,22 +40,6 @@ class ExerciseProgressIndicator extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 10),
-        if (nextExerciseName.isNotEmpty)
-          currentExerciseIndex < totalExercises
-              ? Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    "Prochain exercice : $nextExerciseName",
-                    style: theme.textTheme.headlineSmall,
-                  ),
-                )
-              : Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    nextExerciseName,
-                    style: theme.textTheme.headlineSmall,
-                  ),
-                ),
       ],
     );
   }
