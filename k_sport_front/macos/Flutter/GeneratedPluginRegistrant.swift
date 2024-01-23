a3ad01b6dd02715f9e667342a990c8b2d0d442a6
kevin <kevin-hamza.djeradi@epitech.eu>
2024-01-23 22:17:41 +0100

    configured firebasevfor oush notifs

--- k_sport_front/macos/Flutter/GeneratedPluginRegistrant.swift
+++ k_sport_front/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import firebase_core
+import firebase_messaging
 import flutter_local_notifications
 import flutter_secure_storage_macos
 import flutter_timezone
@@ -13,6 +15,8 @@ import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
