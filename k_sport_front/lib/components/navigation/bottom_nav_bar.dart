fda008b76760aa3e99c5dfc0a00492d095e2d4f5
kevin <kevin-hamza.djeradi@epitech.eu>
2024-01-31 21:06:05 +0100

    removing splash effects from bottom nav bar

--- k_sport_front/lib/components/navigation/bottom_nav_bar.dart
+++ k_sport_front/lib/components/navigation/bottom_nav_bar.dart
@@ -14,37 +14,43 @@ class CustomBottomNavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_box),
-          label: 'Entrainements',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.fitness_center),
-          label: 'Exercices',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Accueil',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.show_chart),
-          label: 'Mes progrès',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profil',
-        ),
-      ],
-      currentIndex: currentIndex,
-      onTap: onTap,
-      selectedItemColor: theme.colorScheme.onSecondary,
-      unselectedItemColor: Colors.grey,
-      backgroundColor: theme.colorScheme.surface,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      type: BottomNavigationBarType.fixed,
+    return Theme(
+      data: Theme.of(context).copyWith(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box),
+            label: 'Entrainements',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercices',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Mes progrès',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: onTap,
+        selectedItemColor: theme.colorScheme.onSecondary,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: theme.colorScheme.surface,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
